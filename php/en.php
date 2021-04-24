cc547a6cbd4f270bf09af939bdc30809e41c61a7
Raggi Izar <61994369+Raggiiz@users.noreply.github.com>
2021-04-24 02:05:51 -0300

    first commit

---
+++ php/en.php
@@ -0,0 +1,163 @@
+<?php
+
+//General 
+
+$back = "Back";
+$lang = '?lang=en';
+$desc = "Presença online é tudo! Por aqui, com as tecnologias mais atuais, criamos sites profissionais e aplicações web e mobile. Também fazemos UI / UX design, branding e hospedagens. Com foco nos detalhes e geração de clientes para o seu negócio!";
+
+//Nav
+
+$nav1 = "Home";
+$nav2 = "About";
+$nav3 = "Service";
+$nav4 = "Works";
+$nav5 = "Testimonial";
+$nav6 = "Contact";
+
+$flag = '<a href="index.html?lang=pt" class="menu_item nav-link slide-horizontal"><img src="img/en.png" alt="guizardev" style="height: 20px;"></a>';
+
+//Landing
+
+$landing_h5 = "Hi There! We are";
+$landing_h1 = "Raggi and João";
+$landing_h4 = "Web and Mobile Developers &amp; Designers";
+
+
+//About
+
+$raggi_title = "Raggi Izar,<br>Front-end Developer &amp; UI / UX Designer";
+$raggi_description = "<span>Developing in
+JavaScript, HTML and CSS.</span>
+Working at L1 SmartSolutions as a Web Developer using Angular, Node.Js, Java and
+PostgreSQL. Currently studying Software Engineer at PUCPR.";
+
+$jogui_title = "João Guis,<br>Full-Stack Developer";
+$jogui_description = "<span>Developing in PHP, Node.Js, PostgreSQL and MySQL.</span>
+I also have knowledge in making <span>Rest API's </span>and in <span>Front-End
+    technologies.</span>
+Working at Nipponsat Solutions I do other stuff like IoT. Currently studying
+Systems Information at PUCPR.";
+
+
+//Services
+
+$services = "Services";
+
+$service_title1 = "Web Development";
+$service_description1 = "Fully responsive web applications and web sites of any kind.";
+
+$service_title2 = "Mobile Apps";
+$service_description2 = "Cutting edge technology of mobile apps for Android and iOS.";
+
+$service_title3 = "UI / UX Design";
+$service_description3 = "User Interface and Experience Design to make best layout to your
+application.";
+
+$service_title4 = "Branding";
+$service_description4 = "Management of your company's brand, such as images, ideas, logos
+and ideas that represent your company.";
+
+$service_title5 = "Hosting";
+$service_description5 = "We host your website with every features you need. Unlimited e-mail accounts, domain and SSL";
+
+$service_title6 = "Support";
+$service_description6 = "Migration to new technologies available, website restructuring and constant updates.";
+
+
+//Works
+
+$works = "Recent Works";
+$about_project = "About project";
+$date = "Date";
+
+$works_filter1 = "Everything";
+$works_filter2 = "Web/Mobile";
+$works_filter3 = "UI/UX";
+$works_filter4 = "Branding";
+
+$work1 = "Look for a Team";
+$work_data1 = "16 June 2018";
+$work1_about = "UI/UX design project that evolve the app's page design, branding, logo, colors, slogan and much more.";
+
+$work2 = "Carolina's Site";
+$work_data2 = "20 Sep 2020";
+$work2_about = "Dr. Carolina Marie's website, with the intuition of publicize your work as complementary natural medical.";
+
+$work3 = "Cabeçote Logo";
+$work_data3 = "13 Dec 2019";
+$work3_about = "E-sports logo made based on Jhin Old West skin from League of Legends.";
+
+$work4 = "E-Parking Applicaiton";
+$work_data4 = "05 Nov 2019";
+
+$work5 = "Mandrakes PUCPR Logo";
+$work_data5 = "16 Aug 2018";
+
+$work6 = "Campeche Bio Solutions Site";
+$work_data6 = "03 Oct 2020";
+
+$work7 = "Trickster SkateShop Site";
+$work_data7 = "10 Apr 2017";
+
+$work8 = "Rocha Martins Site";
+$work_data8 = "25 Sept 2020";
+
+$work9 = "IDP Gaming Logo";
+$work_data9 = "04 Jul 2018";
+
+$work10 = "DaniWash Web Site";
+$work_data10 = "10 Nov 2020";
+$work10_about = "Institucional Web site One page Aplication to Dani Wash project, has been on the market since 2014";
+
+$all_works = "View AllWorks";
+
+$next_page = "Next Page";
+
+//Icons
+
+$icon_title1 = "Happy Clients";
+$icon_title2 = "Project Done";
+$icon_title3 = "Coffee Cups";
+$icon_title4 = "Total Review";
+
+//Testimonials
+
+$testimonials = "Testimonials";
+
+$testimonial1 = "Guizar helped me a lot, provides excellent service to customers and maintains impeccable advice,
+the design of your applications is very beautiful!";
+$testimonial_name1 = "Fernando Risso";
+$testimonial_position1 = "Dev - SaasTec.";
+
+$testimonial2 = "Guizar Dev presents a great management of your company's brand, great hosting services and beautiful attention
+to customers, we love the result of our project.";
+$testimonial_name2 = "Eduardo Gnoatto";
+$testimonial_position2 = "Accountant -  Campeche Bio Solutions.";
+
+$testimonial3 = "They made some really cool effects on Caroline's website, making everything very beautiful, top quality service!";
+$testimonial_name3 = "Julio Martins";
+$testimonial_position3 = "Dev Senior - L1 SmartSolutions.";
+
+//Clients
+
+$clients = "Clients";
+
+
+//Contact
+
+$contact = "Drop A Line";
+
+$phone1 = "Phone 1";
+$phone2 = "Phone 2";
+
+$name = "Name";
+$message = "Tell us about your idea";
+
+$contact_btn = "send message";
+
+//Footer
+
+$footer = "&copy; 2020 <span>GuiZar Development</span>. All Rights Reserved.";
+
+?>
